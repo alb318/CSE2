9d7713acd3793d035708cb1389b9d19ffa6ea751
alb318 <alb318@lehigh.edu>
2015-01-28 04:11:52 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////
+//So this is where I write comments
+//Alex Biester
+//First Homework
+//
+//
+//
+//  class
+public class WelcomeClass{
+
+//  main method
+public static void main (String[] args) {
+    
+// copy the comment
+
+    System.out.println("  -----------");
+    System.out.println("  | Welcome |");
+    System.out.println("  -----------");
+    System.out.println("                    ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-A--L--B--3--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("I have never been to any other country than Canada. I would like to go to New Zealand.");
+ }
+}
